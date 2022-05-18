b12aebb9ed2d384b44382565b8839da07212d12a
Sage Kolpin <sagekolpin@Sages-MacBook-Air.local>
2022-05-17 20:04:10 -0700

    test

--- server.js
+++ server.js
@@ -67,79 +67,79 @@ app.get('/new', (req, res) =>{
   res.render('new.ejs')
 })
 
-// app.get('/collab', (req, res) => {
-//   res.render('collab.ejs')
-// })
+app.get('/collab', (req, res) => {
+  res.render('collab.ejs')
+})
 
-// app.get('/custom', (req, res) => {
-//   res.render('custom.ejs')
-// })
+app.get('/custom', (req, res) => {
+  res.render('custom.ejs')
+})
 
-// app.get('/contact', (req, res) => {
-//   res.render('contact.ejs')
-// })
-// app.get('/about', (req, res) => {
-//   res.render('about.ejs')
-// })
-// app.get('/shadow', (req, res) => {
-//   Data.find({type: "Shadow"}, (err, list) =>{
-//     res.render('shadow.ejs',
-//     {list: list})
-//   })
-// })
+app.get('/contact', (req, res) => {
+  res.render('contact.ejs')
+})
+app.get('/about', (req, res) => {
+  res.render('about.ejs')
+})
+app.get('/shadow', (req, res) => {
+  Data.find({type: "Shadow"}, (err, list) =>{
+    res.render('shadow.ejs',
+    {list: list})
+  })
+})
 
-// app.get('/nature', (req, res) => {
-//   res.render('nature.ejs')
-// })
-// app.get('/blackandwhite', (req, res) => {
-//   res.render('bw.ejs')
-// })
-// app.get('/film', (req, res) => {
-//   res.render('mm.ejs')
-// })
-// app.post('/', (req, res) => {
-//   Data.create(req.body, (err, newPost) => {
-//     res.redirect('/')
-//   })
-// })
+app.get('/nature', (req, res) => {
+  res.render('nature.ejs')
+})
+app.get('/blackandwhite', (req, res) => {
+  res.render('bw.ejs')
+})
+app.get('/film', (req, res) => {
+  res.render('mm.ejs')
+})
+app.post('/', (req, res) => {
+  Data.create(req.body, (err, newPost) => {
+    res.redirect('/')
+  })
+})
  
-//   // show route //
+  // show route //
 
-// app.get('/:id', (req, res) =>{
-//   Data.findById(req.params.id, (err, show) =>{
-//     res.render('show.ejs',
-//     {collect: show})
-//   })
-// })
-
-
+app.get('/:id', (req, res) =>{
+  Data.findById(req.params.id, (err, show) =>{
+    res.render('show.ejs',
+    {collect: show})
+  })
+})
 
 
-// app.get('/:id/edit', (req, res) =>{
-//   Data.findById(req.params.id, (err, edit) =>{
-//     res.render('edit.ejs',
-//     {items: edit})
-//   })
-// })
-// app.put('/:id', (req, res) =>{
-//   Data.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, items) =>{
-//     res.redirect('/')
-//   })
-// })
 
 
-// app.get('/:id', (req, res) => {
-//   Data.findById(req.params.id, (error, view) =>{
-//     res.render('show.ejs',
-//     { items: view })
-//   })
-// })
+app.get('/:id/edit', (req, res) =>{
+  Data.findById(req.params.id, (err, edit) =>{
+    res.render('edit.ejs',
+    {items: edit})
+  })
+})
+app.put('/:id', (req, res) =>{
+  Data.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, items) =>{
+    res.redirect('/')
+  })
+})
 
-// app.delete('/:id', (req, res)=>{
-//   Data.findByIdAndRemove(req.params.id, (err, data)=>{
-//       res.redirect('/');
-//   });
-// });
+
+app.get('/:id', (req, res) => {
+  Data.findById(req.params.id, (error, view) =>{
+    res.render('show.ejs',
+    { items: view })
+  })
+})
+
+app.delete('/:id', (req, res)=>{
+  Data.findByIdAndRemove(req.params.id, (err, data)=>{
+      res.redirect('/');
+  });
+});
 
 
 
